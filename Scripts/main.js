8583a97a844e6af6279d05f6b111664c1e499826
Steven-Leon-007 <estivlinyor@gmail.com>
2022-03-01 00:30:37 -0500

    master - First Commit

---
+++ Scripts/main.js
@@ -0,0 +1,17 @@
+// setTimeout(function moveTextAppear() {
+
+//     document.getElementById('element').style.cssText = `
+//         opacity: 1;
+//         transform: translate(0px,80px);
+//         transition: all 4s ease-in;
+//     `;
+// }, 1000);
+
+// setTimeout(function moveTextDisappear() {
+
+//     document.getElementById('element').style.cssText = `
+//         transform: translate(0px,160px);
+//         opacity: 0;
+//         transition: all 4s ease-out;
+//     `;
+// }, 5000);
