e315e56f7036c3dab47831f75153a4585781f03a
Steven-Leon-007 <estivlinyor@gmail.com>
2022-03-23 20:30:43 -0500

    master - Color changes and pictures

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,35 +1,6 @@
-// setTimeout(function moveTextAppear() {
+let imgPop = document.querySelectorAll('.images-benefits');
+imgPop.forEach(popup => popup.addEventListener('click', () => {
+    popup.classList.toggle('active');
 
-//     document.getElementById('element').style.cssText = `
-//         opacity: 1;
-//         transform: translate(0px,80px);
-//         transition: all 4s ease-in;
-//     `;
-// }, 1000);
-
-// setTimeout(function moveTextDisappear() {
-
-//     document.getElementById('element').style.cssText = `
-//         transform: translate(0px,160px);
-//         opacity: 0;
-//         transition: all 4s ease-out;
-//     `;
-// }, 5000);
-
-// function changePictures () {
-
-//     const pictures = [
-//         '../Pictures/Stranger-Dude-1.png',
-//         '../Pictures/Stranger-Dude-2.png',
-//         '../Pictures/Stranger-Dude-3.png',
-//         '../Pictures/Stranger-Dude-4.png',
-//         '../Pictures/Stranger-Dude-Designer.jpg',
-        
-//     ];
-
-//     const changingImages = document.getElementById('image-1');
-//     const bg = pictures[1];
-//     changingImages.src = bg;
-//     console.log("vrg");
-// }
+}));
 
