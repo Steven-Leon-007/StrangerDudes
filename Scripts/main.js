675c595d780b4e592ed607aeefcd17e11038b471
Steven-Leon-007 <estivlinyor@gmail.com>
2022-04-01 23:28:43 -0500

    master - JS animation and style

--- Scripts/main.js
+++ Scripts/main.js
@@ -17,3 +17,72 @@ function scrollToAppear() {
 }
 
 window.addEventListener('scroll', scrollToAppear);
+
+
+const pictures = [
+	'../Pictures/Stranger-Dude-1.png',
+	'../Pictures/Stranger-Dude-2.png',
+	'../Pictures/Stranger-Dude-3.png',
+	'../Pictures/Stranger-Dude-4.png',
+	'../Pictures/Stranger-Dude-5.png',
+	'../Pictures/Stranger-Dude-6.png',
+	'../Pictures/Stranger-Dude-7.png',
+	'../Pictures/Stranger-Dude-8.png',
+	'../Pictures/Stranger-Dude-9.png',
+	'../Pictures/Stranger-Dude-10.png',
+];
+
+let count = 0;
+
+changePictures = function(){
+	if(count == pictures.length){
+		count = 0;
+	}
+	document.getElementById("image-1").src = pictures[count];
+	count++;
+}
+
+changePictures2 = function(){
+	if(count == pictures.length){
+		count = 0;
+	}
+	document.getElementById("image-2").src = pictures[count];
+	count++;
+}
+
+changePictures3 = function(){
+	if(count == pictures.length){
+		count = 0;
+	}
+	document.getElementById("image-3").src = pictures[count];
+	count++;
+}
+
+changePictures4 = function(){
+	if(count == pictures.length){
+		count = 0;
+	}
+	document.getElementById("image-4").src = pictures[count];
+	count++;
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const asyncAppear = async () => {
+    
+	await delay(1500);
+	changePictures();
+
+	await delay(1500);
+	changePictures2();
+
+	await delay(1500);
+	changePictures3();
+	
+	await delay(1500);
+	changePictures4();
+
+  };
+
+
+setInterval(asyncAppear, 6000);
